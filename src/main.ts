543e3e9f1a5adbbef17f43ea60121cea4aeccea6
dedybastiant <dedybastian.t@gmail.com>
2025-09-05 18:03:55 +0700

    feat: add to app module and main for CheckPhoneNumber endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { globalValidationPipe } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(globalValidationPipe);
+
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
