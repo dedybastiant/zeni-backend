1422a60803f058e84cbc45c83ffd849272a794db
dedybastiant <dedybastian.t@gmail.com>
2025-09-05 18:18:39 +0700

    feat: add length validation for phone number

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,5 @@
 import { Expose } from 'class-transformer';
-import { IsDefined, IsNotEmpty, IsNumberString } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsNumberString, Length } from 'class-validator';
 import { BaseResponseDto } from 'src/common/dto/response.dto';
 
 export class CheckPhoneNumberDto {
@@ -7,6 +7,7 @@ export class CheckPhoneNumberDto {
   @IsNumberString({}, { message: 'Phone number must contain only digits' })
   @IsDefined({ message: 'Phone number cannot be empty' })
   @IsNotEmpty({ message: 'Phone number cannot be empty' })
+  @Length(10, 15, { message: 'Phone number must be between 10 and 15 digits' })
   phoneNumber: string;
 }
 
