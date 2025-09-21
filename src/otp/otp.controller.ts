b4f60e9342dc0a033ed1b9df18575892085b8515
dedybastiant <dedybastian.t@gmail.com>
2025-09-21 16:44:41 +0700

    feat: add registration guards token and using on the send otp API

--- src/otp/otp.controller.ts
+++ src/otp/otp.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { OtpService } from './otp.service';
 import { GenerateOtpRequestDto } from './dto/otp.dto';
+import { RegistrationTokenGuard } from 'src/common/guards/registration-token.guard';
 
 @Controller('v1/otp')
 export class OtpController {
   constructor(private readonly otpService: OtpService) {}
 
+  @UseGuards(RegistrationTokenGuard)
   @Post('/send-otp')
   async generateAndSendOtp(
     @Body() generateOtpRequestDto: GenerateOtpRequestDto,
