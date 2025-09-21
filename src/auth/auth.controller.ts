223c71064ccf9f478516b233f1769e89dd4d941d
dedybastiant <dedybastian.t@gmail.com>
2025-09-21 14:40:06 +0700

    refactor: remove unused decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,8 @@
-import {
-  Body,
-  ClassSerializerInterceptor,
-  Controller,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CheckPhoneNumberRequestDto } from './dto/auth.dto';
 
 @Controller('v1/auth')
-@UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
