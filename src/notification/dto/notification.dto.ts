a6960dc9c3631721c80a2d53149b7591d8407ea6
dedybastiant <dedybastian.t@gmail.com>
2025-09-25 21:55:15 +0700

    feat: add dummy send email verification function

--- src/notification/dto/notification.dto.ts
+++ src/notification/dto/notification.dto.ts
@@ -1,4 +1,6 @@
 import { OtpChannel, OtpType } from '@prisma/client';
+import { IsDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { BaseResponseDto } from 'src/common/dto/base-response.dto';
 
 export class SendOtpRequestDto {
   phoneNumber: string;
@@ -7,3 +9,17 @@ export class SendOtpRequestDto {
   channel: OtpChannel;
   type: OtpType;
 }
+
+export class SendEmailVerificationRequestDto {
+  @IsDefined({ message: 'Email is required' })
+  @IsNotEmpty({ message: 'Email cannot be blank' })
+  @IsEmail({}, { message: 'Invalid email format' })
+  email: string;
+
+  @IsDefined({ message: 'Token is required' })
+  @IsNotEmpty({ message: 'Token cannot be blank' })
+  @IsString({ message: 'Token should be a string' })
+  token: string;
+}
+
+export class SendEmailVerificationResponseDto extends BaseResponseDto {}
